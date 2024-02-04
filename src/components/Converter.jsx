1f270572858a83690493f276dea31ae12f946301
winta38 <winta38@gmail.com>
2024-02-05 00:47:22 +0500

    fixed the error output

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,28 +1,34 @@
 import { useState } from "react";
 
 export default function Converter() {
-    const [hex, setHex] = useState('')
+    const [hex, setHex] = useState('');
 
     const handleChange = (event) => {
-        const { value } = event.target
-        if (value.length === 7) {
+        const { value } = event.target;
+
+        if (value.length > 7) {
+            setHex('Error');
+        } else if (value.length === 7) {
             const result = /^#([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
+
             if (result === null) {
                 setHex('Error');
-                return
-            }
-            const rgb = `${parseInt(result[1], 16)},
-          ${parseInt(result[2], 16)},
-          ${parseInt(result[3], 16)}`;
-            setHex(`rgb(${rgb})`);
-        } else if (value.length > 7) {
-            setHex('Error');
-            return
-        }
-    }
+            } else {
+                const rgb = `${parseInt(result[1], 16)},
+                     ${parseInt(result[2], 16)},
+                     ${parseInt(result[3], 16)}`;
 
-    return <div className='container' style={{ background: hex }}>
-        <input className='converter' type="text" onChange={handleChange} />
-        <div className='result'>{hex}</div>
-    </div>
+                setHex(`rgb(${rgb})`);
+            }
+        } else {
+            setHex('');
+        }
+    };
+
+    return (
+        <div className='container' style={{ background: hex }}>
+            <input className='converter' type="text" maxLength={7} onChange={handleChange} />
+            <div className='result'>{hex}</div>
+        </div>
+    );
 }
